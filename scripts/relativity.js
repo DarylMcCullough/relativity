eb0a225dd701508bbbbf09d879c84afd3a220f70
dmccullough <dmccullough@decisionpatterns.com>
2016-12-21 10:14:31 -0500

    refactor to allow switch between frames without restarting

--- scripts/relativity.js
+++ scripts/relativity.js
@@ -1,24 +1,25 @@
 function Ship(leftClock, rightClock, leftSpacer, rod, name) {
-    this.leftClock = leftClock;
-    this.rightClock = rightClock;
-    this.spacer = leftSpacer;
-    this.rod = rod;
-    this.leftTime = 0;
-    this.rightTime = 0;
-    this.clockWidth = 60;
-    this.clockHeight = 60;
-    this.rodHeight = 10;
-    this.rodLength = 160;
-    this.leftPosition = 0;
-    this.rightPosition = 0;
-    this.clockRate = 1;
-    this.name = name;
-    this.setSize = function(x) {
+    this.leftClock = leftClock; // a div representing the clock on the left end of the ship
+    this.rightClock = rightClock; // a div representing the clock on the right end of the ship
+    this.spacer = leftSpacer; // a div to the left of the ship; its left margin determines the position of the ship
+    this.rod = rod; // a div representing a rod connecting the left and right clocks
+    this.leftTime = 0; // the current time on the left clock, in minutes past 12:00
+    this.rightTime = 0; // the current time on the right clock, in minutes past 12:00
+    this.clockWidth = 60; // the size of each clock, in px
+    this.clockHeight = 70; // the height of each clock in px
+    this.rodHeight = 10; // the height of the rod in px
+    this.rodLength = 160; // the length of the rod in px
+    this.leftPosition = 0; // the position (from the left of the window) of the center of the left clock
+    this.rightPosition = 0; // the position (from the left of the window) of the center of the right clock
+    this.clockRate = 1.0; // the clock rate (minutes per "tick")
+    this.name = name; // the name of the ship (not used for anything)
+    this.setSize = function(x) { // sets the length of the rod
         this.rodLength = x;
         this.rod.style.width = x + "px";
     };
     
-    this.positionLeft = function(x) {
+    this.positionLeft = function(x) { // sets the value of this.leftPosition (and calculates this.rightPosition) 
+                                      // by adjusting the margin of the spacer
         if (x*2 < this.clockWidth) {
             x = Math.floor((this.clockWidth + 1)/2);
         }
@@ -28,28 +29,19 @@ function Ship(leftClock, rightClock, leftSpacer, rod, name) {
         this.rightPosition = x + this.clockWidth + this.rodLength;
     };
     
-    this.positionRight = function(x) {
+    this.positionRight = function(x) { // sets the value of this.rightPosition (and calculates this.leftPosition) 
+                                      // by adjusting the margin of the spacer
         var y = x - this.clockWidth - this.rodLength;
         this.positionLeft(y);
     };
     
-    this.renderClocks = function() {
+    this.renderClocks = function() { // converts times on clocks to the format "hh:mm" and displays result in the corresponding divs
         this.leftClock.textContent = this.renderTime(this.leftTime);
         this.rightClock.textContent = this.renderTime(this.rightTime);
     }
     
-    this.updateClocks = function() {
-        this.leftTime += this.clockRate;
-        this.rightTime += this.clockRate;
-        this.renderClocks();
-        this.ticks++;
-        if (this.ticks % 400 == 0) {
-            //alert("ticks: " + this.ticks + "time: " + this.leftClock.textContent);
-        }
-    };
-    
-    this.renderTime = function(t) {
-        t = Math.floor(t);
+    this.renderTime = function(t) { // converts times in minutes past 12:00 to "hh:mm" format
+        t = Math.floor(t + 0.5);
         var retval = ""
         var minutes = t;
         var hour = 12;
@@ -85,19 +77,20 @@ function Ship(leftClock, rightClock, leftSpacer, rod, name) {
 
 }
 
-var ships = [];
+var ships = []; // Will hold the two ships
 
-var shortLength = 70;
-var longLength = 200;
-var startPosition = 400;
-var shortRate = 0.115;
-var longRate = 0.230;
-var pos_delta = 500;
-var red_move = false;
-var pos_max = 450;
-var pos = 0;
+var shortLength = 70; // the length of the length-contracted ship's rod
+var longLength = 200; // the length of the uncontracted ship's rod
+var startPosition = 400; // the starting position (in px) of the left clock of the green ship and right clock o red ship
+var shortRate = 0.115; // the clock rate of the time-dilated clock
+var longRate = 0.230; // the clock rate of the undilated clock
+var pos_delta = 100; // extra padding on left, in px
+var red_move = false; // if true, then red ship is moving, green ship is stationary. If false, red ship is stationary, green ship is moving
+var pos_max = 450; // maximum number of "ticks" to run simulation
+var pos = 0; // current number of ticks
+var time_offset = 45; // offset of left clock of red ship when it is moving, or right clock of green ship when it is moving
 
-window.onload = function() {
+window.onload = function() { // create ships, initialize their lengths, positions, clocks, etc.
     var spacer = document.getElementById("spacer2");
     var leftClock = document.getElementById("one");
     var rightClock = document.getElementById("two");
@@ -115,17 +108,123 @@ window.onload = function() {
     initGreen();
 }
 
-function stopMove() {
+function setUpShips(ticks, red_move1) { // calculate ship parameters for given number of ticks; red_move1 specifies whether the red ship is moving
+    if (ticks === undefined || ticks < 0) {
+        if (red_move1 != red_move) {
+            ticks = Math.floor((ships[0].ticks / longRate) * shortRate + 0.5);
+        } else {
+            ticks = ships[0].ticks;
+        }
+    }
+    
+    red_move = red_move1;
+
+    if (red_move) {
+        pos_delta = 100;
+        ships[0].setSize(shortLength);
+        ships[0].clockRate = shortRate;
+        ships[0].rightTime = ticks * shortRate;
+        ships[0].leftTime = ticks * shortRate + time_offset;
+        ships[0].renderClocks();
+        var position = startPosition + pos_delta + ticks;
+        ships[0].ticks = ticks;
+        ships[0].positionRight(position);
+        
+        ships[1].setSize(longLength);
+        ships[1].clockRate = longRate;
+        ships[1].leftTime = ticks * longRate;
+        ships[1].rightTime = ticks * longRate;
+        ships[1].positionLeft(startPosition + pos_delta);
+        ships[1].renderClocks();
+        ships[1].ticks = ticks;
+        pos = ticks;
+    } else {
+        pos_delta = 100;
+        ships[0].setSize(longLength);
+        ships[0].clockRate = longRate;
+        ships[0].rightTime = ticks * longRate;
+        ships[0].leftTime = ticks * longRate;
+        ships[0].renderClocks();
+        var position = startPosition + pos_delta;
+        ships[0].ticks = ticks;
+        ships[0].positionRight(position);
+        
+        ships[1].setSize(shortLength);
+        ships[1].clockRate = longRate;
+        ships[1].leftTime = ticks * shortRate;
+        ships[1].rightTime = ticks * shortRate + time_offset;
+        var position = startPosition + pos_delta - ticks;
+        ships[1].positionLeft(position);
+        ships[1].renderClocks();
+        ships[1].ticks = ticks;
+        pos = ticks;
+    }
+}
+
+function setUpShips(ticks, red_move1) { // calculate ship parameters for given number of ticks; red_move1 specifies whether the red ship is moving
+    if (ticks === undefined || ticks < 0) {
+        if (red_move1 != red_move) {
+            ticks = Math.floor((ships[0].ticks / longRate) * shortRate + 0.5);
+        } else {
+            ticks = ships[0].ticks;
+        }
+    }
+    
+    red_move = red_move1;
+
+    if (red_move) {
+        pos_delta = 100;
+        ships[0].setSize(shortLength);
+        ships[0].clockRate = shortRate;
+        ships[0].rightTime = ticks * shortRate;
+        ships[0].leftTime = ticks * shortRate + time_offset;
+        ships[0].renderClocks();
+        var position = startPosition + pos_delta + ticks;
+        ships[0].ticks = ticks;
+        ships[0].positionRight(position);
+        
+        ships[1].setSize(longLength);
+        ships[1].clockRate = longRate;
+        ships[1].leftTime = ticks * longRate;
+        ships[1].rightTime = ticks * longRate;
+        ships[1].positionLeft(startPosition + pos_delta);
+        ships[1].renderClocks();
+        ships[1].ticks = ticks;
+        pos = ticks;
+    } else {
+        pos_delta = 100;
+        ships[0].setSize(longLength);
+        ships[0].clockRate = longRate;
+        ships[0].rightTime = ticks * longRate;
+        ships[0].leftTime = ticks * longRate;
+        ships[0].renderClocks();
+        var position = startPosition + pos_delta;
+        ships[0].ticks = ticks;
+        ships[0].positionRight(position);
+        
+        ships[1].setSize(shortLength);
+        ships[1].clockRate = longRate;
+        ships[1].leftTime = ticks * shortRate;
+        ships[1].rightTime = ticks * shortRate + time_offset;
+        var position = startPosition + pos_delta - ticks;
+        ships[1].positionLeft(position);
+        ships[1].renderClocks();
+        ships[1].ticks = ticks;
+        pos = ticks;
+    }
+}
+
+function stopMove() { // stops animation
     if (id >= 0) {
         clearInterval(id);
     }
 }
 
-function initGreen() {
+function initGreen() { // sets up simulation for green frame (red ship moving)
     pos_delta = 100;
     ships[0].setSize(shortLength);
     ships[0].clockRate = shortRate;
-    ships[0].leftTime = 45;
+    ships[0].leftTime = time_offset;
     ships[0].rightTime = 0;
     ships[0].renderClocks();
     var position = startPosition + pos_delta;
@@ -142,7 +241,23 @@ function initGreen() {
     pos = 0;
 }
 
-function initRed() {
+function switchToGreen() { // switches to green frame, keeping the time on the left green clock unchanged
+    //alert("ships[0]: " + ships[0].ticks + "ticks, " + ships[0].rightTime + "time");
+    //alert("ships[1]: " + ships[1].ticks + "ticks, " + ships[1].leftTime + "time");
+    setUpShips(-1, true);
+    //alert("ships[0]: " + ships[0].ticks + "ticks, " + ships[0].rightTime + "time");
+    //alert("ships[1]: " + ships[1].ticks + "ticks, " + ships[1].leftTime + "time");
+}
+
+function switchToRed() { // switches to red frame, keeping time on the right red clock unchanged
+    //alert("ships[0]: " + ships[0].ticks + "ticks, " + ships[0].rightTime + "time");
+    //alert("ships[1]: " + ships[1].ticks + "ticks, " + ships[1].leftTime + "time");
+    setUpShips(-1, false);
+    //alert("ships[0]: " + ships[0].ticks + "ticks, " + ships[0].rightTime + "time");
+    //alert("ships[1]: " + ships[1].ticks + "ticks, " + ships[1].leftTime + "time");
+}
+
+function initRed() { // sets up simulation for red frame (green ship moving) 
     pos_delta = 100;
     ships[0].setSize(longLength);
     ships[0].clockRate = longRate;
@@ -153,7 +268,7 @@ function initRed() {
     ships[0].leftTime = 0;
     ships[0].rightTime = 0;
     ships[1].leftTime = 0;
-    ships[1].rightTime = 45;
+    ships[1].rightTime = time_offset;
     ships[0].renderClocks();
     ships[1].renderClocks();
     red_move = false;
@@ -168,22 +283,7 @@ function myMove() {
             clearInterval(id)
         } else {
             pos++;
-            if (red_move) {
-                var position = pos + startPosition + pos_delta;
-                if (! done) {
-                }
-                ships[0].positionRight(position);
-                if (! done) {
-                    //alert("hey");
-                }
-                done = true;
-            } else {
-                
-                ships[1].positionLeft(startPosition + pos_delta - pos );
-
-            }
-            ships[0].updateClocks();
-            ships[1].updateClocks();
+            setUpShips(pos, red_move);
         }
         
     }
